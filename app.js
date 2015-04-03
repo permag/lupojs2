e3b7d18e3a0a345f99d00a1f972c74fbb92a1ca3
Per Magnusson <killingfloor00@gmail.com>
2015-04-03 15:15:35 +0200

    Fixes and updates in lib and code init new components.

--- app.js
+++ app.js
@@ -2,25 +2,43 @@ $(function () {
     /// APP
     var myComp = lupo.comps.myComp(),
         testComp = lupo.comps.testComp(),
+        listComp = lupo.comps.listComp(),
+        itemComp = lupo.comps.itemComp(),
         testModel = {
-        firstname: 'John',
-        surname: 'Smith',
-        description: '<h3>A short text...</h3>',
-        list: [
-            {item: 'item 1'},
-            {item: 'item 2'},
-            {item: 'item 3'}
-        ],
-        deep: {
-            ob: {
-                list: [
-                    {article: 'article 1'},
-                    {article: 'article 2'},
-                    {article: 'article 3'}
-                ]
+            firstname: 'John',
+            surname: 'Smith',
+            description: '<h3>A short text...</h3>',
+            list: [
+                {item: 'item 1'},
+                {item: 'item 2'},
+                {item: 'item 3'}
+            ],
+            deep: {
+                ob: {
+                    list: [
+                        {article: 'article 1'},
+                        {article: 'article 2'},
+                        {article: 'article 3'}
+                    ]
+                }
             }
-        }
-    };
+        },
+        listModel = {
+            list: [
+                {
+                    id: 1,
+                    firstname: 'Homer',
+                    surname: 'Simpson',
+                    age: 33
+                },
+                {
+                    id: 2,
+                    firstname: 'Peter',
+                    surname: 'Griffin',
+                    age: 41
+                }
+            ]
+        };
 
     // Routes
     lupo.router({
@@ -28,7 +46,9 @@ $(function () {
             myCompRoute();
         },
         '/mycomp': myCompRoute,
-        '/testcomp': testCompRoute
+        '/testcomp': testCompRoute,
+        '/list': listCompRoute,
+        '/item/:id': itemCompRoute
     });
 
     // Route functions
@@ -38,5 +58,15 @@ $(function () {
 
     function testCompRoute() {
         testComp.render({view: 'app-view'});
+    }
+
+    function listCompRoute() {
+        listComp.render({model: listModel});
+    }
+
+    function itemCompRoute(id) {
+        var modelItem = lupo.getObjectItem(listModel.list, 'id', id);
+        itemComp.render({model: modelItem});
+
     }
 });
